f621082dbb03119ab3a54868362427cefe0960c3
Joshita <joshita.atmakuri@gmail.com>
2021-09-07 14:32:59 +0530

    tech news

---
+++ app/src/main/java/com/example/technewsapp/splash_screen.java
@@ -0,0 +1,46 @@
+package com.example.technewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash_screen extends AppCompatActivity {
+    private static int timer=4000;
+    ImageView logo;
+    TextView textview;
+    Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        logo=findViewById(R.id.logo);
+        textview=findViewById(R.id.textView2);
+        animation= AnimationUtils.loadAnimation(this,R.anim.animation);
+        logo.setAnimation(animation);
+        textview.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash_screen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },timer);
+
+
+
+
+
+    }
+}
